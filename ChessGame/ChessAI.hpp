d7f92a0fba1f0bb3720bb204b046fed1f2ed8bba
Ethan Bechtol <48114346+EthanBechtol@users.noreply.github.com>
2020-03-17 23:01:10 -0700

    Add core abstract class for a ChessAI and a ChessMove structure

---
+++ ChessGame/ChessAI.hpp
@@ -0,0 +1,26 @@
+//
+// Created by Ethan on 2/24/20.
+//
+
+#ifndef CHESSPLUSPLUS_CHESSAI_HPP
+#define CHESSPLUSPLUS_CHESSAI_HPP
+
+
+#include <utility>
+#include "ChessGameState.hpp"
+#include "ChessMove.hpp"
+
+// A ChessAI is currently intended to be a *stateless* class that may be constructed and called upon at any time to make
+// a move given solely the current state of the game in the form of a ChessGameState.
+
+class ChessAI {
+    virtual ~ChessAI() = default;
+
+    // choseMove() returns the best move this AI believes is the best. With that move, the game manager will make update
+    // the current game state to reflect the move returned by this function. It is assumed that this function will only
+    // be called upon when there is a move to make.
+    virtual ChessMove choseMove(const ChessGameState& state) = 0;
+};
+
+
+#endif //CHESSPLUSPLUS_CHESSAI_HPP
---
+++ ChessGame/ChessMove.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Ethan on 3/17/20.
+//
+
+#ifndef CHESSPLUSPLUS_CHESSMOVE_HPP
+#define CHESSPLUSPLUS_CHESSMOVE_HPP
+
+struct ChessMove {
+    std::pair<int, int> start;
+    std::pair<int, int> end;
+};
+
+#endif //CHESSPLUSPLUS_CHESSMOVE_HPP
