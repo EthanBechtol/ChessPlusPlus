37a7e6794b571a8b51fcfe6a943476b3f75f661c
Ethan Bechtol <48114346+EthanBechtol@users.noreply.github.com>
2020-04-30 20:10:38 -0700

    Cleanup main with temporary AI proof of concept showcase

--- main.cpp
+++ main.cpp
@@ -1,43 +1,47 @@
 #include <iostream>
-#include <sstream>
+#include <chrono>
 #include "StandardChess/StandardChessBoard.hpp"
 #include "StandardChess/StandardChessGameState.hpp"
 #include "ChessGame/ChessMove.hpp"
+#include "ChessGame/ChessException.hpp"
+#include "StandardChess/StandardChessAIs/RandomAI.hpp"
+#include "StandardChess/StandardChessAIs/MinMaxAI.hpp"
 
 
-ChessMove lineToMove(std::istringstream& line){
-    ChessMove move;
-
-    line >> move.start.first >> move.start.second >> move.end.first >> move.end.second;
-    return move;
+template<typename T1, typename T2>
+std::string pairToStr(const std::pair<T1, T2>& pair) {
+    return "(" + std::to_string(pair.first) + ", " + std::to_string(pair.second) + ")";
 }
+
 
 int main() {
     StandardChessBoard board;
-//    board.outputBoard(std::cout);
-
     StandardChessGameState gameState(std::move(board));
 
-    std::string line;
 
-    gameState.board().outputBoard(std::cout);
-    std::getline(std::cin, line);
-    //std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    // Proof of concept Min/Max AI showcase vs Random AI
+    while(gameState.whiteScore() < 10 && gameState.blackScore() < 10){
+        std::string AIName;
+        ChessMove AIMove;
 
-    while(line.rfind("quit") != 0){
-        std::istringstream input(line);
-        ChessMove requestedMove = lineToMove(input);
+        std::chrono::steady_clock::time_point startTime = std::chrono::steady_clock::now();
+        if(gameState.isWhiteTurn()) {
+            RandomAI ai;
+            AIName = ai.AIName;
+            AIMove = ai.choseMove(gameState);
+        }
+        else {
+            MinMaxAI ai;
+            AIName = ai.AIName;
+            AIMove = ai.choseMove(gameState);
+        }
+        std::chrono::steady_clock::time_point endTime = std::chrono::steady_clock::now();
+        std::chrono::duration<double, std::milli> elapsedTime = endTime - startTime;
 
-        gameState.makeMove(requestedMove.start, requestedMove.end);
-
+        std::cout << AIName << " chose move: " + pairToStr(AIMove.start) + " -> " + pairToStr(AIMove.end) << " in " << elapsedTime.count() << "ms" << std::endl;
+        gameState.makeMove(AIMove.start, AIMove.end);
         gameState.board().outputBoard(std::cout);
-        std::getline(std::cin, line);
-        //std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
-
-    // TODO IMPORTANT StdChessGameState ->
-    //  different moves are allowed depending on whether or not a piece is being taken.
-    //  AND whether or not they are allowed to take that piece (i.e. white can't take white)
 
     return 0;
 }
